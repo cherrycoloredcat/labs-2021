95994f2dd2424c200dffc801dc889ee80784a515
cherrycoloredcat <vladimir_a_telnov@mail.ru>
2021-02-10 19:42:58 +0500

    lab1 arrays code

---
+++ lab1/lab1_arrays/lab1_arrays/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace lab1_arrays
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(12, 145);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(609, 99);
+            this.textBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите через пробел массив целых чисел";
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(12, 250);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(609, 46);
+            this.calculateButton.TabIndex = 2;
+            this.calculateButton.Text = "Посчитать";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(433, 65);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Вариант 25\r\n\r\nУсловие: \r\nДан одномерный целочисленный массив порядка N. Найти кол" +
+    "ичество элементов \r\nмассива меньше среднего гармонического всех элементов массив" +
+    "а.\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 323);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox);
+            this.Name = "Form1";
+            this.Text = "lab1: arrays";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ lab1/lab1_arrays/lab1_arrays/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace lab1_arrays
+{
+    public partial class Form1 : Form
+    {
+        private int[] _numsToCalculate;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            string[] rawNums = textBox.Text.Trim().Split(new char[] { ' ', '\r', '\n' });          // Формирование массива чисел из textBox
+
+            try
+            {
+                _numsToCalculate = (from num in rawNums where !string.IsNullOrWhiteSpace(num) select Convert.ToInt32(num)).ToArray();    // Конвертация в int
+            }
+            catch
+            {
+                MessageBox.Show("Недопустимый ввод", "Ошибка");
+                return;
+            }
+
+            float harmonicMean = calculateHarmonicMean();
+
+            int amount = 0;
+
+            foreach (int num in _numsToCalculate)                       // Подсчет и вывод
+                if (num < harmonicMean)
+                    amount++;
+
+            MessageBox.Show("Среднее гармоническое - " + harmonicMean +
+                "\n\nКоличество элементов, удовлетворяющих условию - " + amount, "Результат");
+        }
+
+        private float calculateHarmonicMean()           // Расчет среднего гармонического
+        {
+            float sum = 0;
+
+            foreach(int num in _numsToCalculate)
+            {
+                sum += 1.0f / num;
+            }
+
+            return _numsToCalculate.Length / sum;
+        }
+    }
+}
