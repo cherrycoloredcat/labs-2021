9828dbbe6bb4376e87ee8212a6aa969db4477e9e
cherrycoloredcat <vladimir_a_telnov@mail.ru>
2021-02-16 21:18:07 +0500

    lab1_strings code

---
+++ lab1/lab1_strings/lab1_strings/FormStringReplace.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace lab1_strings
+{
+    partial class FormStringReplace
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxFirstAlphabet = new System.Windows.Forms.TextBox();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.textBoxSecondAlphabet = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttonReplace = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(12, 229);
+            this.textBoxInput.Multiline = true;
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(663, 110);
+            this.textBoxInput.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(660, 68);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Вариант 25\r\n\r\nУсловие:\r\nФункция заменяет символы строки из одного заданного алфав" +
+    "ита на символы другого алфавита.\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(9, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Входная строка";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(9, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(463, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Введите через пробел символы алфавита, которые будут заменены\r\n";
+            // 
+            // textBoxFirstAlphabet
+            // 
+            this.textBoxFirstAlphabet.Location = new System.Drawing.Point(12, 112);
+            this.textBoxFirstAlphabet.Name = "textBoxFirstAlphabet";
+            this.textBoxFirstAlphabet.Size = new System.Drawing.Size(660, 20);
+            this.textBoxFirstAlphabet.TabIndex = 0;
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(12, 375);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.ReadOnly = true;
+            this.textBoxOutput.Size = new System.Drawing.Size(663, 110);
+            this.textBoxOutput.TabIndex = 0;
+            // 
+            // textBoxSecondAlphabet
+            // 
+            this.textBoxSecondAlphabet.Location = new System.Drawing.Point(12, 174);
+            this.textBoxSecondAlphabet.Name = "textBoxSecondAlphabet";
+            this.textBoxSecondAlphabet.Size = new System.Drawing.Size(660, 20);
+            this.textBoxSecondAlphabet.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(9, 355);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 17);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Измененная строка";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(9, 154);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(670, 17);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Введите через пробел алфавит, на который соответсвенно будут заменены символы пер" +
+    "дыдущего";
+            // 
+            // buttonReplace
+            // 
+            this.buttonReplace.Location = new System.Drawing.Point(12, 510);
+            this.buttonReplace.Name = "buttonReplace";
+            this.buttonReplace.Size = new System.Drawing.Size(663, 50);
+            this.buttonReplace.TabIndex = 3;
+            this.buttonReplace.Text = "Заменить";
+            this.buttonReplace.UseVisualStyleBackColor = true;
+            this.buttonReplace.Click += new System.EventHandler(this.buttonReplace_Click);
+            // 
+            // FormStringReplace
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 575);
+            this.Controls.Add(this.buttonReplace);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxSecondAlphabet);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.textBoxFirstAlphabet);
+            this.Controls.Add(this.textBoxInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(705, 618);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(705, 618);
+            this.Name = "FormStringReplace";
+            this.Text = "lab1: strings";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxFirstAlphabet;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.TextBox textBoxSecondAlphabet;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button buttonReplace;
+    }
+}
+
---
+++ lab1/lab1_strings/lab1_strings/FormStringReplace.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace lab1_strings
+{
+    public partial class FormStringReplace : Form
+    {
+        private char[] _firstAlphabet, _secondAlphabet;
+
+        private string _modifiedString;
+        public FormStringReplace()
+        {
+            InitializeComponent();
+        }
+
+        // Вызывается событием нажатия кнопки "Заменить"
+        // и преобразует строку
+        private void buttonReplace_Click(object sender, EventArgs e)
+        {
+            if (!ParseAlphabets())
+                return;
+
+            // Преобразование строки
+
+            char[] inputSting = textBoxInput.Text.ToCharArray();
+
+            for(int i = 0; i < inputSting.Length; i++)
+            {
+                for(int j = 0; j < _firstAlphabet.Length; j++)
+                {
+                    if(inputSting[i] == _firstAlphabet[j])
+                    {
+                        inputSting[i] = _secondAlphabet[j];
+                        break;
+                    }
+                        
+                }
+            }
+
+            _modifiedString = new string(inputSting);
+
+            textBoxOutput.Text = _modifiedString;
+
+            MessageBox.Show("Строка преобразована успешно\n", "Успех");
+        }
+
+
+        // Преобразует строки, введенные в первые 2 поля в алфавиты
+        // Возвращает true при успешном преобразовании и false при ошибке
+        private bool ParseAlphabets()
+        {
+            string[] firstAlphabetUnparsed, secondAlphabetUnparsed;
+
+            firstAlphabetUnparsed = textBoxFirstAlphabet.Text.Trim().Split(' ');
+            secondAlphabetUnparsed = textBoxSecondAlphabet.Text.Trim().Split(' ');
+
+            if(string.IsNullOrWhiteSpace(textBoxFirstAlphabet.Text))
+            {
+                MessageBox.Show("Введен пустой алфавит", "Ошибка");
+                return false;
+            }
+
+            if (firstAlphabetUnparsed.Length != secondAlphabetUnparsed.Length)
+            {
+                MessageBox.Show("Введены алфавиты разной длины,\nлибо введен двойной символ пробела", "Ошибка");
+                return false;
+            }
+
+            _firstAlphabet = new char[firstAlphabetUnparsed.Length];
+            _secondAlphabet = new char[secondAlphabetUnparsed.Length];
+
+            for (int i = 0; i < firstAlphabetUnparsed.Length; i++)
+            {
+                if (string.IsNullOrWhiteSpace(firstAlphabetUnparsed[i]) || string.IsNullOrWhiteSpace(secondAlphabetUnparsed[i]))
+                {
+                    MessageBox.Show("Введен двойной символ пробела", "Ошибка");
+                    return false;
+                }
+
+                if (firstAlphabetUnparsed[i].Length > 1 || secondAlphabetUnparsed[i].Length > 1)
+                {
+                    MessageBox.Show("Элемент алфавита содержит больше одного символа", "Ошибка");
+                    return false;
+                }
+
+                _firstAlphabet[i] = firstAlphabetUnparsed[i][0];
+                for(int j = 0; j < i; j++)
+                {
+                    if(_firstAlphabet[j] == _firstAlphabet[i])
+                    {
+                        MessageBox.Show("Обнаружен повторяющийся символ в первом алфавите", "Ошибка");
+                        return false;
+                    }
+
+                }
+
+                _secondAlphabet[i] = secondAlphabetUnparsed[i][0];
+            }
+
+            return true;
+        }
+    }
+}
