2d93f48c96da5296c27940e3f70129f426ad1678
cherrycoloredcat <vladimir_a_telnov@mail.ru>
2021-05-30 17:39:10 +0500

    lab6

---
+++ lab6/lab6/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace lab6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.textBoxSize = new System.Windows.Forms.TextBox();
+            this.textBoxKey = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.buttonTree = new System.Windows.Forms.Button();
+            this.textBoxRemove = new System.Windows.Forms.TextBox();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.buttonFind = new System.Windows.Forms.Button();
+            this.textBoxWord = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(489, 12);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.ReadOnly = true;
+            this.textBoxOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxOutput.Size = new System.Drawing.Size(299, 426);
+            this.textBoxOutput.TabIndex = 0;
+            // 
+            // textBoxSize
+            // 
+            this.textBoxSize.Location = new System.Drawing.Point(12, 29);
+            this.textBoxSize.Name = "textBoxSize";
+            this.textBoxSize.Size = new System.Drawing.Size(116, 20);
+            this.textBoxSize.TabIndex = 1;
+            // 
+            // textBoxKey
+            // 
+            this.textBoxKey.Location = new System.Drawing.Point(12, 78);
+            this.textBoxKey.Name = "textBoxKey";
+            this.textBoxKey.Size = new System.Drawing.Size(116, 20);
+            this.textBoxKey.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Размер";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ключ";
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(162, 27);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(306, 71);
+            this.buttonSearch.TabIndex = 3;
+            this.buttonSearch.Text = "Поиск";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // buttonTree
+            // 
+            this.buttonTree.Location = new System.Drawing.Point(162, 141);
+            this.buttonTree.Name = "buttonTree";
+            this.buttonTree.Size = new System.Drawing.Size(306, 56);
+            this.buttonTree.TabIndex = 4;
+            this.buttonTree.Text = "Создать дерево";
+            this.buttonTree.UseVisualStyleBackColor = true;
+            this.buttonTree.Click += new System.EventHandler(this.buttonTree_Click);
+            // 
+            // textBoxRemove
+            // 
+            this.textBoxRemove.Location = new System.Drawing.Point(12, 141);
+            this.textBoxRemove.Name = "textBoxRemove";
+            this.textBoxRemove.Size = new System.Drawing.Size(116, 20);
+            this.textBoxRemove.TabIndex = 1;
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Location = new System.Drawing.Point(12, 167);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(116, 30);
+            this.buttonRemove.TabIndex = 5;
+            this.buttonRemove.Text = "Удалить";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // buttonFind
+            // 
+            this.buttonFind.Location = new System.Drawing.Point(162, 241);
+            this.buttonFind.Name = "buttonFind";
+            this.buttonFind.Size = new System.Drawing.Size(306, 56);
+            this.buttonFind.TabIndex = 4;
+            this.buttonFind.Text = "Найти слово в файле";
+            this.buttonFind.UseVisualStyleBackColor = true;
+            this.buttonFind.Click += new System.EventHandler(this.buttonFind_Click);
+            // 
+            // textBoxWord
+            // 
+            this.textBoxWord.Location = new System.Drawing.Point(12, 241);
+            this.textBoxWord.Name = "textBoxWord";
+            this.textBoxWord.Size = new System.Drawing.Size(116, 20);
+            this.textBoxWord.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.buttonFind);
+            this.Controls.Add(this.buttonTree);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxRemove);
+            this.Controls.Add(this.textBoxWord);
+            this.Controls.Add(this.textBoxKey);
+            this.Controls.Add(this.textBoxSize);
+            this.Controls.Add(this.textBoxOutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.TextBox textBoxSize;
+        private System.Windows.Forms.TextBox textBoxKey;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonTree;
+        private System.Windows.Forms.TextBox textBoxRemove;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.Button buttonFind;
+        private System.Windows.Forms.TextBox textBoxWord;
+    }
+}
+
---
+++ lab6/lab6/Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab6
+{
+    public partial class Form1 : Form
+    {
+        private BinarySearchTree _bst;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            int size = Convert.ToInt32(textBoxSize.Text);
+            int key = Convert.ToInt32(textBoxKey.Text);
+
+            int[] input = Generate(size);
+
+            int [] data = LinearSearch(input, key);
+
+            string output = "Найдено " + data.Length + " симоволов " + key + " на позициях ";
+
+            for(int i = 0; i < data.Length; i++)
+            {
+                output += data[i] + " ";
+            }
+
+            output += Environment.NewLine + Environment.NewLine;
+
+            foreach(int i in input)
+            {
+                output += i + " ";
+            }
+
+            textBoxOutput.Text = output;
+
+        }
+
+        private int[] Generate(int size)
+        {
+            int[] arr = new int[size];
+
+            Random rnd = new Random();
+
+            for(int i = 0; i < size; i++)
+            {
+                arr[i] = rnd.Next(-100, 100);
+            }
+
+            return arr;
+        }
+
+        private int[] LinearSearch(int[] input, int key)
+        {
+            List<int> res = new List<int>();
+
+            for(int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == key)
+                    res.Add(i);
+            }
+
+            return res.ToArray();
+        }
+
+        private void buttonTree_Click(object sender, EventArgs e)
+        {
+            _bst = new BinarySearchTree();
+
+            int[] input = Generate(100);
+
+            foreach(int i in input)
+            {
+                _bst.Add(i);
+            }
+
+            textBoxOutput.Text = _bst.Print();
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            int value = Convert.ToInt32(textBoxRemove.Text);
+
+            _bst.Remove(value);
+
+            textBoxOutput.Text = _bst.Print();
+        }
+
+        private void buttonFind_Click(object sender, EventArgs e)
+        {
+            string text;
+
+            using(StreamReader sr = new StreamReader("text.txt"))
+            {
+                text = sr.ReadToEnd();
+            }
+
+            int ind = KMP(text, textBoxWord.Text);
+
+            textBoxOutput.Text = ind.ToString();
+
+        }
+
+        private int KMP(string text, string word)
+        {
+            int[] indexes = new int[word.Length];
+
+            bool same;
+
+            for(int i = 1; i < word.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    same = true;
+
+                    for (int k = 0; k <= j; k++)
+                    {
+                        if (word[k] != word[i - j + k])
+                        {
+                            same = false;
+                            break;
+                        }
+                    }
+
+                    if (same)
+                        indexes[i] = j + 1;
+                }
+            }
+
+            int word_ind = 0;
+
+            for(int i = 0; i < text.Length; i++)
+            {
+
+                if (word_ind == word.Length)
+                    return i - word_ind;
+
+                if (text[i] == word[word_ind])
+                {
+                    word_ind++;
+
+                }
+                else
+                {
+                    if(word_ind != 0)
+                    {
+                        word_ind = indexes[word_ind - 1];
+                        i--;
+                    }
+                }
+            }
+
+            if (word_ind == word.Length)
+                return text.Length - word_ind;
+            else
+                return -1;
+
+        }
+    }
+
+    public class BinarySearchTree
+    {
+        public Node Head { get; private set; }
+
+        public void Add(int value)
+        {
+            Node n = new Node(value);
+
+            if (Head == null)
+            {
+                Head = n;
+                return;
+            }
+
+            Node closest;
+
+            SearchForTheClosest(value, out closest);
+
+            if (closest.Key == value)
+                return;
+
+            if (closest.Key < value)
+                closest.Right = n;
+            else
+                closest.Left = n;
+        }
+
+        public void Remove(int value)
+        {
+            Node target, upper;
+
+            SearchForTheClosest(value, out target, out upper);
+
+            if (target.Key != value)
+                return;
+
+            if(target.Right == null)
+            {
+                if (upper.Left == target)
+                    upper.Left = null;
+                else
+                    upper.Right = null;
+            }
+            else
+            {
+                Node pr_swap = target;
+                Node swap = target.Right;
+
+                while (swap.Left != null)
+                {
+                    pr_swap = swap;
+                    swap = swap.Left;
+                }
+
+                target.Key = swap.Key;
+
+                pr_swap.Left = swap.Right;
+
+            }
+
+
+        }
+
+        private void SearchForTheClosest(int key, out Node target, out Node upper )
+        {
+            target = null;
+            upper = null;
+
+            if (Head == null)
+            {
+                return;
+            }
+
+            target = Head;
+
+            while(target.Key < key && target.Right != null || target.Key > key && target.Left != null)
+            {
+                if (target.Key == key)
+                    break;
+
+                upper = target;
+
+                if (target.Key > key)
+                    target = target.Left;
+
+                else
+                    target = target.Right;
+
+            }
+        }
+
+        private void SearchForTheClosest(int key, out Node target)
+        {
+            Node upper = null;
+
+            target = null;
+
+            SearchForTheClosest(key, out target, out upper);
+        }
+
+        public string Print()
+        {
+            return PrintStep(Head, "");
+        }
+
+        private string PrintStep(Node cur, string str)
+        {
+            str += cur.Key + " ";
+
+            string output = "", t;
+
+            if (cur.Left != null)
+                output += PrintStep(cur.Left, str);
+
+            if (cur.Right != null)
+                output += PrintStep(cur.Right, str);
+
+            if (cur.Left == null && cur.Right == null)
+                return str + Environment.NewLine;
+            else
+                return output;
+
+        }
+
+    }
+
+
+    public class Node
+    {
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+        public int Key { get; set; }
+
+        public Node(int key)
+        {
+            Key = key;
+        }
+    }
+}
